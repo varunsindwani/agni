32918271be21a62e2ff68d787731ddf9276d2d3c
Varun Sindwani <varun@sindwani.com>
2019-11-06 05:11:37 -0500

    colors added

---
+++ src/components/ToolBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ToolBar = () => (
+  <nav className="flex ">
+    {/* colors */}
+    <button className="bg-black py-4 px-4 w-full max-w-full"></button>
+    <button className="bg-white py-4 px-4 w-full max-w-full"></button>
+    <button className="bg-gray-500 py-4 px-4 w-full max-w-full"></button>
+    <button className="bg-red-500 py-4 px-4 w-full max-w-full"></button>
+    <button className="bg-orange-500 py-4 px-4 w-full max-w-full"></button>
+    <button className="bg-yellow-500 py-4 px-4 w-full max-w-full"></button>
+    <button className="bg-green-500 py-4 px-4 w-full max-w-full"></button>
+    <button className="bg-teal-500 py-4 px-4 w-full max-w-full"></button>
+    <button className="bg-blue-500 py-4 px-4 w-full max-w-full"></button>
+    <button className="bg-indigo-500 py-4 px-4 w-full max-w-full"></button>
+    <button className="bg-purple-500 py-4 px-4 w-full max-w-full"></button>
+    <button className="bg-pink-500 py-4 px-4 w-full max-w-full"></button>
+  </nav>
+);
+
+export default ToolBar;
