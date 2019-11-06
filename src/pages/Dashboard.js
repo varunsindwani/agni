64f3b325c4c74d02b829b13bc3a77bcdc2051c3b
Varun Sindwani <varun@sindwani.com>
2019-11-06 05:10:48 -0500

    cleaned and canvas component added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from '../components/Layout'
+import Canvas from '../components/Canvas'
+
 
 function Dashboard() {
   return (
     <Layout>
-      {/* insert the logo image here */}
+      <Canvas />
     </Layout>
   );
 }
